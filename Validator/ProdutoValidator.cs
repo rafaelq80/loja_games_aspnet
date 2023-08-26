3ad32f5ec6d544f39b913f2592d48f86b8d76342
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-08-26 00:52:01 -0300

    Adicionar arquivos de projeto.

---
+++ Validator/ProdutoValidator.cs
@@ -0,0 +1,28 @@
+﻿using loja_games.Model;
+using FluentValidation;
+
+namespace loja_games.Validator
+{
+    public class ProdutoValidator : AbstractValidator<Produto>
+    {
+
+        public ProdutoValidator()
+        {
+
+            RuleFor(p => p.Nome)
+                .NotEmpty();
+
+            RuleFor(p => p.Descricao)
+                .NotEmpty();
+
+            RuleFor(p => p.Console)
+                .NotEmpty();
+
+            RuleFor(p => p.Preco)
+                .NotNull()
+                .GreaterThan(0);
+
+        }
+
+    }
+}
